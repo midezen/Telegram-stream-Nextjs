c95d4bd1b801a0b8ac36a5ee8195c267466f9980
Ayomide <victordiya90@gmail.com>
2025-09-26 12:35:12 +0100

    set up clerk and convex

---
+++ components/ConvexClientProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ReactNode } from "react";
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error("Missing NEXT_PUBLIC_CONVEX_URL in your .env file");
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
+const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
+};
+
+export default ConvexClientProvider;
